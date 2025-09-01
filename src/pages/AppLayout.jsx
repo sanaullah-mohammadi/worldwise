1478970424ff4c999d6431b8e30e26b4e8409e3b
sanaullah <sanaullahmohammadi554@gmail.com>
2025-09-01 09:00:36 +0430

    some file is added to component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import AppNav from "../Components/AppNav";
 function AppLayout() {
   return (
     <div>
+      <AppNav />
       <p>some important thing is added to here</p>
     </div>
   );
